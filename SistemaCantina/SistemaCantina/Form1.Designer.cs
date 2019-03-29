d5743e895be13690e9122e28f245fce8d6a7cbd3
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-29 12:24:09 -0300

    Sistema Cantina em C#
    Programa de Sistema Cantina básico em c#

---
+++ SistemaCantina/SistemaCantina/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SistemaCantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.listCaixa = new System.Windows.Forms.ListBox();
+            this.lblTitulo3 = new System.Windows.Forms.Label();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.lblTitulo4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(91, 29);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(185, 22);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "SISTEMA CANTINA";
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.BackColor = System.Drawing.Color.Transparent;
+            this.lblProduto.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.Location = new System.Drawing.Point(27, 96);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(152, 20);
+            this.lblProduto.TabIndex = 1;
+            this.lblProduto.Text = "CÓDIGO DO PRODUTO:";
+            // 
+            // textCodigo
+            // 
+            this.textCodigo.Location = new System.Drawing.Point(31, 119);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(150, 20);
+            this.textCodigo.TabIndex = 2;
+            this.textCodigo.TextChanged += new System.EventHandler(this.textCodigo_TextChanged);
+            // 
+            // listCaixa
+            // 
+            this.listCaixa.FormattingEnabled = true;
+            this.listCaixa.Location = new System.Drawing.Point(198, 96);
+            this.listCaixa.Name = "listCaixa";
+            this.listCaixa.Size = new System.Drawing.Size(151, 173);
+            this.listCaixa.TabIndex = 4;
+            // 
+            // lblTitulo3
+            // 
+            this.lblTitulo3.AutoSize = true;
+            this.lblTitulo3.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo3.Location = new System.Drawing.Point(124, 305);
+            this.lblTitulo3.Name = "lblTitulo3";
+            this.lblTitulo3.Size = new System.Drawing.Size(0, 17);
+            this.lblTitulo3.TabIndex = 5;
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.BackColor = System.Drawing.Color.Transparent;
+            this.BtnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(263, 315);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(86, 29);
+            this.BtnSair.TabIndex = 6;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = false;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(31, 145);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(150, 124);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagem.TabIndex = 7;
+            this.picImagem.TabStop = false;
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.Location = new System.Drawing.Point(263, 275);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(86, 27);
+            this.BtnLimpar.TabIndex = 8;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // lblTitulo4
+            // 
+            this.lblTitulo4.AutoSize = true;
+            this.lblTitulo4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo4.Location = new System.Drawing.Point(8, 305);
+            this.lblTitulo4.Name = "lblTitulo4";
+            this.lblTitulo4.Size = new System.Drawing.Size(110, 17);
+            this.lblTitulo4.TabIndex = 9;
+            this.lblTitulo4.Text = "VALOR TOTAL:";
+            this.lblTitulo4.Click += new System.EventHandler(this.lblTitulo4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(377, 356);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblTitulo4);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.lblTitulo3);
+            this.Controls.Add(this.listCaixa);
+            this.Controls.Add(this.textCodigo);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema Cantina";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.ListBox listCaixa;
+        private System.Windows.Forms.Label lblTitulo3;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Label lblTitulo4;
+    }
+}
+
---
+++ SistemaCantina/SistemaCantina/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCantina
+{
+    public partial class Form1 : Form
+    {
+        /* Declaração de 3 arrays para armazenar 6 produtos cada um e seus respectivos códigos e valores. */
+
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma = 0;
+
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void textCodigo_TextChanged(object sender, EventArgs e)
+        {
+            /*inicia com uma condição, se o usuário digitar um código com 5 caracteres (length), esse código é adicionado ao listBox. 
+              A propriedade length retorna a quantidade de caracteres.*/ 
+
+            if(textCodigo.Text.Length == 3)
+           
+
+            {
+                int indice = 0;
+                for (int prod = 1; prod < codigo.Length; prod++ )
+
+                /*Faça enquanto o prod for menor ou igual a qtde de itens do array */
+                {
+                    if(textCodigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+
+                    }
+                }
+
+               if(indice == 0)
+               {
+                   MessageBox.Show("Produto não encontrado");
+               }
+
+                   /*Condição, se foi encontrado o produto ele retorna as informações de acordo com o índice 
+                     caso não encontre aparecerá a mensagem, Produto não encontrado*/
+
+                else
+               {
+                   listCaixa.Items.Add(textCodigo.Text + "--" + produtos[indice] + "--R$ " + valor[indice]);
+
+                   /* Conteúdo adicionado no ListBox, concatena (+) as informaçôes (código/nome/valor)*/
+                  
+                   soma = soma + valor[indice];
+                   lblTitulo3.Text = ("R$: " + soma);
+
+                   picImagem.ImageLocation = "c:/imagem/" + codigo[indice] + ".jpg";
+                   textCodigo.Text = "";
+                   textCodigo.Focus();
+
+                   /*localização da imagem que deve ser colocada em uma pasta em c: em uma pasta imagens otextbox
+                    será limpo e o Focus posiciona o cursorpara o TextBox para uma nova digitação*/
+
+
+
+               }
+            } 
+
+               
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Ao entrar no Form Load será chamado o método*/
+            
+            carregarArray();
+            soma = 0;
+
+
+        }
+        /*Ao entra no from load será chamando o método */ 
+
+        private void carregarArray()
+        {
+            /*Criação do Método para ser chamado quando necessário, 
+             isso evita repetir várias vezes o mesmo código*/
+
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+
+            produtos[1] = "Pastel";
+            produtos[2] = "Coxinha";
+            produtos[3] = "Hot_Dog";
+            produtos[4] = "Chocolate";
+            produtos[5] = "suco";
+
+            valor[1] = 6.00;
+            valor[2] = 5.00;
+            valor[3] = 12.00;
+            valor[4] = 3.50;
+            valor[5] = 8.00;
+        }
+
+        /*Códigos, Produtos e Valores ref. os arrays que foram declarados*/
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            //Declarando codigo Sair close
+
+            Close();
+
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            //Declarando o codigo Limpar , listCaixa, picImage, lbTitulo3.text
+
+            listCaixa.Items.Clear();
+            picImagem.Image = null;
+            lblTitulo3.Text = " ";
+
+
+            
+            
+
+
+
+        }
+
+        private void lblTitulo4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
